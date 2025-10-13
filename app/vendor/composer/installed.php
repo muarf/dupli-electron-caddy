bc90dfcf79b76eef239cf44d20d97f0d9e37adea
muarf <muarf@riseup.net>
2025-10-13 13:12:39 -0400

    Update from dupli-php-dev

--- app/vendor/composer/installed.php
+++ app/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'dupli-dev/app',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5655fe8d1c5539f9ce3408871c0b570b7f477189',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'dupli-dev/app' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5655fe8d1c5539f9ce3408871c0b570b7f477189',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
