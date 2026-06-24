47817d13a14edac66ed1dbb35f26ffecae8c6c42
muarf <muarf@users.noreply.github.com>
2026-06-24 11:30:34 +0200

    feat(tauri): require administrator privileges on Windows startup via app manifest in build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -81,7 +81,42 @@ fn main() {
         }
     }
 
-    tauri_build::build();
+    #[cfg(target_os = "windows")]
+    {
+        let mut windows = tauri_build::WindowsAttributes::new();
+        windows = windows.app_manifest(
+            r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <dependency>
+    <dependentAssembly>
+      <assemblyIdentity
+        type="win32"
+        name="Microsoft.Windows.Common-Controls"
+        version="6.0.0.0"
+        processorArchitecture="*"
+        publicKeyToken="6595b64144ccf1df"
+        language="*"
+      />
+    </dependentAssembly>
+  </dependency>
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+  </trustInfo>
+</assembly>
+"#
+        );
+        tauri_build::try_build(tauri_build::Attributes::new().windows_attributes(windows))
+            .expect("failed to run build script");
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    {
+        tauri_build::build();
+    }
 }
 
 fn copy_dir_all(src: impl AsRef<std::path::Path>, dst: impl AsRef<std::path::Path>) -> std::io::Result<()> {
